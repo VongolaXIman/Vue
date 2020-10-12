a7f15616cb56547aa404f2b620b3fbd98fa48e47
Wilson <wilson40609@gmail.com>
2020-10-12 14:54:08 +0800

    Initial

---
+++ learning/Project/vue-first/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-alert': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': process.env.NODE_ENV === 'production' ? ['error', 'windows'] :
+    'off',
+    'space-before-function-paren': process.env.NODE_ENV === 'production' ? ['error',
+    'never'] : 'off',
+    'import/extensions': process.env.NODE_ENV === 'production' ? ['error', 'never'] :
+    'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
